068059887d8ba8cf2fdef0b35bc4adf21862ea84
cosmicdisaster <44583599+cosmicdisaster@users.noreply.github.com>
2020-09-15 20:40:15 +0530

    Update vector-erase.cpp

--- C++/vector-erase.cpp
+++ C++/vector-erase.cpp
@@ -6,6 +6,8 @@ erase(int start,int end):
 
 Removes the elements in the range from start to end inclusive of the start and exclusive of the end.
 Ex:v.erase(v.begin()+2,v.begin()+5);(erases all the elements from the third element to the fifth element.)
+sorting in vectors
+sort(v.begin(),v.end());
 */
 
 #include <cmath>
